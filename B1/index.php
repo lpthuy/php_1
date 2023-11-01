16b41fd58ad3a854fbee35068e7a7185a8c6cd4e
lpthuy <133140353+lpthuy@users.noreply.github.com>
2023-11-01 21:09:39 +0700

    Nodung

---
+++ B1/index.php
@@ -0,0 +1,7 @@
+<?php 
+$a = 10 ;
+$b =20;
+echo "a có giá trị là ".$a."</br>";
+$sum = $a + $b;
+echo "tổng là :".$sum;
+?>
---
+++ demo/B1/index.php
@@ -0,0 +1,20 @@
+<?php 
+$a = 10 ;
+$b =20;
+echo "a có giá trị là :".$a."</br>";
+echo "b có giá trị là :".$b."</br>";
+
+$sum = $a + $b;
+echo "tổng a và b là :".$sum."</br>";
+$tru = $a - $b;
+echo "hiệu a và b là :".$tru."</br>";
+$tich= $a * $b;
+echo "tích a và b là :".$tich."</br>";
+$du= $b % $a;
+echo "dư b chia a là :".$du."</br>";
+$tang = $a++;
+echo "a tăng là :".$tang."</br>";
+$giam = $a--;
+echo " a giảm là :".$giam."</br>";
+
+?>
